e6a10d7f7de7fd4537b77e0ed86f65caebfbde05
Diana Pajón <deedeemusso@gmail.com>
2018-10-11 10:59:46 -0300

    Reformulados los levels, ahora actualLevel lleva solamente track del scope.

--- src/TigerSres.hs
+++ src/TigerSres.hs
@@ -3,18 +3,18 @@ module TigerSres where
 import TigerTips
 import TigerTemp
 import TigerUnique
---import TigerTrans
---import TigerFrame
+import TigerTrans
+import TigerFrame
 
 -- | 'Externa' representa la idea si una función pertenece al /runtime/ o no.
 data Externa = Runtime | Propia
     deriving Show
 
-type FunEntry = (Unique, Label, [Tipo], Tipo, Externa) -- Entrega 1
---type FunEntry = (Level, Label, [Tipo], Tipo, Externa) --Entrega 2
+--type FunEntry = (Unique, Label, [Tipo], Tipo, Externa) -- Entrega 1
+type FunEntry = (Level, Label, [Tipo], Tipo, Externa) --Entrega 2
 
-type ValEntry = Tipo --Entrega 1
---type ValEntry =  (Tipo, Access, Int) --Entrega 2
+--type ValEntry = Tipo --Entrega 1
+type ValEntry =  (Tipo, Access, Int) --Entrega 2
 
 data EnvEntry =
     Var ValEntry | Func FunEntry
